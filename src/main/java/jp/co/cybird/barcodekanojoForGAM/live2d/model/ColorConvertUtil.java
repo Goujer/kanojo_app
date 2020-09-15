a3e0cc65e49111b8aa33d5c0820fed2c3c04f099
Goujer <legojr33@gmail.com>
2020-09-15 16:00:21 -0500

    Fix #36 Artifacts on darker kanojos gone
    Slight rework of color changer also included. Might even be faster now.

--- src/main/java/jp/co/cybird/barcodekanojoForGAM/live2d/model/ColorConvertUtil.java
+++ src/main/java/jp/co/cybird/barcodekanojoForGAM/live2d/model/ColorConvertUtil.java
@@ -1,194 +1,173 @@
 package jp.co.cybird.barcodekanojoForGAM.live2d.model;
 
-import android.support.v4.view.MotionEventCompat;
-
 public class ColorConvertUtil {
-    static final int COLOR_CONV_PERCENT = 0;
-    static final int COLOR_SATULATION_ADJUST = 1;
+	static final int COLOR_CONV_PERCENT = 0;
+	static final int COLOR_SATULATION_ADJUST = 1;
 
-    public static void convertGray(int[] imageDataInt, int grayValue, int width, int height, int texSizeW, int texSizeH) {
-        for (int y = 0; y < height; y++) {
-            int offy = y * texSizeW;
-            for (int x = 0; x < width; x++) {
-                int off = x + offy;
-                int a = (imageDataInt[off] >> 24) & MotionEventCompat.ACTION_MASK;
-                if (a > 0) {
-                    int gv = grayValue;
-                    imageDataInt[off] = ((a << 24) & -16777216) | ((gv << 16) & 16711680) | ((gv << 8) & MotionEventCompat.ACTION_POINTER_INDEX_MASK) | (gv & MotionEventCompat.ACTION_MASK);
-                }
-            }
-        }
-    }
+	public static void convertGray(int[] imageDataInt, int grayValue, int width, int height, int texSizeW, int texSizeH) {
+		for (int y = 0; y < height; y++) {
+			int offy = y * texSizeW;
+			for (int x = 0; x < width; x++) {
+				int off = x + offy;
+				int a = (imageDataInt[off] >> 24) & 255;
+				if (a > 0) {
+					imageDataInt[off] = ((a << 24) & -16777216) | ((grayValue << 16) & 16711680) | ((grayValue << 8) & 65280) | (grayValue & 255);
+				}
+			}
+		}
+	}
 
-    static void convertColor_exe1(int[] imageDataInt, ColorConvert colorConvert, int width, int height, int texSizeW, int texSizeH) {
-        float hsl_S;
-        float h;
-        float hsl_H;
-        float f;
-        float hsl_S2;
-        float f2;
-        float cmax;
-        float cmin;
-        float dr;
-        float dg;
-        float db;
-        float conv_H = colorConvert.hue;
-        float conv_S = colorConvert.sat;
-        float conv_L = colorConvert.bri;
-        float lastSR = -1.0f;
-        float lastSG = -1.0f;
-        float lastSB = -1.0f;
-        float lastSA = -1.0f;
-        int lastDR = -1;
-        int lastDG = -1;
-        int lastDB = -1;
-        for (int y = 0; y < height; y++) {
-            int offy = y * texSizeW;
-            for (int x = 0; x < width; x++) {
-                int off = x + offy;
-                int v = imageDataInt[off];
-                int a = (v >> 24) & MotionEventCompat.ACTION_MASK;
-                float sa = 0.003921569f * ((float) a);
-                if (((double) sa) >= 0.1d) {
-                    float sr = (float) ((v >> 16) & MotionEventCompat.ACTION_MASK);
-                    float sg = (float) ((v >> 8) & MotionEventCompat.ACTION_MASK);
-                    float sb = (float) (v & MotionEventCompat.ACTION_MASK);
-                    if (sr == lastSR && sg == lastSG && sb == lastSB && sa == lastSA) {
-                        imageDataInt[off] = ((a << 24) & -16777216) | ((lastDR << 16) & 16711680) | ((lastDG << 8) & MotionEventCompat.ACTION_POINTER_INDEX_MASK) | (lastDB & MotionEventCompat.ACTION_MASK);
-                    } else {
-                        lastSR = sr;
-                        lastSG = sg;
-                        lastSB = sb;
-                        lastSA = sa;
-                        float sr2 = sr * 0.003921569f;
-                        float sg2 = sg * 0.003921569f;
-                        float sb2 = sb * 0.003921569f;
-                        float cmax2 = sr2 >= sg2 ? sr2 > sb2 ? sr2 : sb2 : sg2 > sb2 ? sg2 : sb2;
-                        float cmin2 = sr2 <= sg2 ? sr2 < sb2 ? sr2 : sb2 : sg2 < sb2 ? sg2 : sb2;
-                        float hsl_L = (cmax2 + cmin2) - 1.0f;
-                        float cd = cmax2 - cmin2;
-                        if (cd == 0.0f) {
-                            hsl_S2 = 0.0f;
-                            hsl_H = 0;
-                        } else {
-                            if (hsl_L <= 0.0f) {
-                                hsl_S = cd / (cmax2 + cmin2);
-                            } else {
-                                hsl_S = cd / (2.0f - (cmax2 + cmin2));
-                            }
-                            if (sr2 == cmax2) {
-                                h = (sg2 - sb2) / cd;
-                            } else if (sg2 == cmax2) {
-                                h = 2.0f + ((sb2 - sr2) / cd);
-                            } else {
-                                h = 4.0f + ((sr2 - sg2) / cd);
-                            }
-                            float h2 = h * 60.0f;
-                            if (h2 < 0.0f) {
-                                h2 += 360.0f;
-                            }
-                            hsl_H = h2;
-                            if (hsl_L < 0.0f) {
-                                f = -hsl_L;
-                            } else {
-                                f = hsl_L;
-                            }
-                            hsl_S2 = hsl_S * (1.0f - f);
-                        }
-                        float shusendo = (-0.5f - hsl_L) * 3.3333333f;
-                        if (shusendo <= 1.0f) {
-                            float hsl_H2 = hsl_H + conv_H;
-                            float hsl_S3 = hsl_S2 + conv_S;
-                            float hsl_L2 = hsl_L + conv_L;
-                            if (hsl_L2 < 0.0f) {
-                                f2 = -hsl_L2;
-                            } else {
-                                f2 = hsl_L2;
-                            }
-                            float div = 1.0f - f2;
-                            if (div != 0.0f) {
-                                hsl_S3 /= div;
-                            }
-                            if (hsl_L2 < -1.0f) {
-                                hsl_L2 = -1082130432;
-                            }
-                            if (hsl_L2 > 1.0f) {
-                                hsl_L2 = 1.0f;
-                            }
-                            if (hsl_S3 < 0.0f) {
-                                hsl_S3 = 0.0f;
-                            }
-                            if (hsl_S3 > 1.0f) {
-                                hsl_S3 = 1.0f;
-                            }
-                            float hsl_H3 = hsl_H2 - ((float) (((int) (hsl_H2 / 360.0f)) * 360));
-                            if (hsl_H3 < 0.0f) {
-                                hsl_H3 += 360.0f;
-                            }
-                            if (hsl_L2 <= 0.0f) {
-                                cmin = ((1.0f + hsl_L2) / 2.0f) * (1.0f - hsl_S3);
-                                cmax = (1.0f + hsl_L2) - cmin;
-                            } else {
-                                cmax = (((1.0f + hsl_L2) / 2.0f) * (1.0f - hsl_S3)) + hsl_S3;
-                                cmin = (1.0f + hsl_L2) - cmax;
-                            }
-                            float tmph = hsl_H3 + 120.0f;
-                            float tmph2 = tmph - ((float) (((int) (tmph / 360.0f)) * 360));
-                            if (tmph2 < 0.0f) {
-                                tmph2 += 360.0f;
-                            }
-                            if (tmph2 < 60.0f) {
-                                dr = cmin + (((cmax - cmin) * tmph2) / 60.0f);
-                            } else if (tmph2 < 180.0f) {
-                                dr = cmax;
-                            } else if (tmph2 < 240.0f) {
-                                dr = cmin + (((cmax - cmin) * (240.0f - tmph2)) / 60.0f);
-                            } else {
-                                dr = cmin;
-                            }
-                            float tmph3 = hsl_H3;
-                            float tmph4 = tmph3 - ((float) (((int) (tmph3 / 360.0f)) * 360));
-                            if (tmph4 < 0.0f) {
-                                tmph4 += 360.0f;
-                            }
-                            if (tmph4 < 60.0f) {
-                                dg = cmin + (((cmax - cmin) * tmph4) / 60.0f);
-                            } else if (tmph4 < 180.0f) {
-                                dg = cmax;
-                            } else if (tmph4 < 240.0f) {
-                                dg = cmin + (((cmax - cmin) * (240.0f - tmph4)) / 60.0f);
-                            } else {
-                                dg = cmin;
-                            }
-                            float tmph5 = hsl_H3 - 120.0f;
-                            float tmph6 = tmph5 - ((float) (((int) (tmph5 / 360.0f)) * 360));
-                            if (tmph6 < 0.0f) {
-                                tmph6 += 360.0f;
-                            }
-                            if (tmph6 < 60.0f) {
-                                db = cmin + (((cmax - cmin) * tmph6) / 60.0f);
-                            } else if (tmph6 < 180.0f) {
-                                db = cmax;
-                            } else if (tmph6 < 240.0f) {
-                                db = cmin + (((cmax - cmin) * (240.0f - tmph6)) / 60.0f);
-                            } else {
-                                db = cmin;
-                            }
-                            if (shusendo < 0.0f) {
-                                lastDR = (int) (255.0f * dr);
-                                lastDG = (int) (255.0f * dg);
-                                lastDB = (int) (255.0f * db);
-                                imageDataInt[off] = ((a << 24) & -16777216) | ((lastDR << 16) & 16711680) | ((lastDG << 8) & MotionEventCompat.ACTION_POINTER_INDEX_MASK) | (lastDB & MotionEventCompat.ACTION_MASK);
-                            } else {
-                                lastDR = (int) ((sr2 * shusendo) + (255.0f * dr * (1.0f - shusendo)));
-                                lastDG = (int) ((sg2 * shusendo) + (255.0f * dg * (1.0f - shusendo)));
-                                lastDB = (int) ((sb2 * shusendo) + (255.0f * db * (1.0f - shusendo)));
-                                imageDataInt[off] = ((a << 24) & -16777216) | ((lastDR << 16) & 16711680) | ((lastDG << 8) & MotionEventCompat.ACTION_POINTER_INDEX_MASK) | (lastDB & MotionEventCompat.ACTION_MASK);
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
+	//Have no illusions this thing follows no standard for HSL that I can find. That said Hue and Lum seems correct but I have no idea what's happening with Sat.
+	static void convertColor_exe1(int[] imageDataInt, ColorConvert colorConvert, int width, int height, int texSizeW, int texSizeH) {
+		int lastA = -1;
+		int lastR = -1;
+		int lastG = -1;
+		int lastB = -1;
+
+		int lastDR = -1;
+		int lastDG = -1;
+		int lastDB = -1;
+
+		for (int y = 0; y < height; y++) {
+			int offy = y * texSizeW;
+			for (int x = 0; x < width; x++) {
+				int off = x + offy;
+				int v = imageDataInt[off];
+				int a = (v >> 24) & 0xff;
+
+				if (a >= 26) {	//Process conversion if alpha [0,255] is greater than 26
+					int r = (v >> 16) & 0xff;
+					int g = (v >> 8) & 0xff;
+					int b = v & 0xff;
+					if (r != g || r != b) {
+						if (r == lastR && g == lastG && b == lastB && a == lastA) {
+							imageDataInt[off] = ((a << 24) & 0xff000000) | ((lastDR << 16) & 0xff0000) | ((lastDG << 8) & 0xff00) | (lastDB & 0xff);
+						} else {
+							lastA = a;
+							lastR = r;
+							lastG = g;
+							lastB = b;
+
+							float sr = r / 255.0f;
+							float sg = g / 255.0f;
+							float sb = b / 255.0f;
+
+							float cmax2 = sr >= sg ? Math.max(sr, sb) : Math.max(sg, sb);
+							float cmin2 = sr <= sg ? Math.min(sr, sb) : Math.min(sg, sb);
+							float hsl_H;
+							float hsl_S;
+							float hsl_L = (cmax2 + cmin2) / 2.0f;	//RGB to Lum
+							float chroma = cmax2 - cmin2;
+
+							float shusendo = -6.6666666f*hsl_L - 1.6666666f;
+							if (shusendo <= 1.0f) {
+								if (sr == cmax2) {	//RGB to Hue
+									hsl_H = 60.0f * ((sg - sb) / chroma);
+								} else if (sg == cmax2) {
+									hsl_H = 60.0f * (2.0f + ((sb - sr) / chroma));
+								} else {
+									hsl_H = 60.0f * (4.0f + ((sr - sg) / chroma));
+								}
+
+								if (hsl_L <= 0.5f) {	//RGB to Sat
+									hsl_S = chroma / (cmax2 + cmin2);
+								} else {
+									hsl_S = chroma / (2.0f - (cmax2 + cmin2));
+								}
+								hsl_S *= 1.0f - Math.abs(2.0f*hsl_L-1);
+
+								//Alter values.
+								hsl_H += colorConvert.hue;
+								hsl_S += colorConvert.sat;
+								hsl_L += colorConvert.lum;
+								float dr, dg, db;
+
+								//Sat Correction
+								float div = 1.0f - Math.abs(2.0f*hsl_L-1);
+								if (div != 0.0f) {
+									hsl_S /= div;
+								}
+								if (hsl_S < 0.0f) {
+									hsl_S = 0.0f;
+								} else if (hsl_S > 1.0f) {
+									hsl_S = 1.0f;
+								}
+
+								//Lum Correction
+								if (hsl_L < 0.0f) {
+									hsl_L = 0.0f;
+								} else if (hsl_L > 1.0f) {
+									hsl_L = 1.0f;
+								}
+
+								//Hue Correction
+								hsl_H %= 360.0f;
+								if (hsl_H < 0.0f) {
+									hsl_H += 360.0f;
+								}
+
+								float cmax, cmin;
+								if (hsl_L <= 0.5f) {
+									cmin = hsl_L * (1.0f - hsl_S);
+									cmax = 2.0f*hsl_L - cmin;
+								} else {
+									cmax = hsl_L * (1.0f - hsl_S) + hsl_S;
+									cmin = 2.0f*hsl_L - cmax;
+								}
+
+								//Red
+								float tmph = (hsl_H + 120.0f) % 360.0f;
+								if (tmph < 60.0f) {
+									dr = cmin + (((cmax - cmin) * tmph) / 60.0f);
+								} else if (tmph < 180.0f) {
+									dr = cmax;
+								} else if (tmph < 240.0f) {
+									dr = cmin + (((cmax - cmin) * (240.0f - tmph)) / 60.0f);
+								} else {
+									dr = cmin;
+								}
+
+								//Green
+								if (hsl_H < 60.0f) {
+									dg = cmin + (((cmax - cmin) * hsl_H) / 60.0f);
+								} else if (hsl_H < 180.0f) {
+									dg = cmax;
+								} else if (hsl_H < 240.0f) {
+									dg = cmin + (((cmax - cmin) * (240.0f - hsl_H)) / 60.0f);
+								} else {
+									dg = cmin;
+								}
+
+								//Blue
+								tmph = hsl_H - 120.0f;
+								if (tmph < 0.0f) {
+									tmph += 360.0f;
+								}
+								if (tmph < 60.0f) {
+									db = cmin + (((cmax - cmin) * tmph) / 60.0f);
+								} else if (tmph < 180.0f) {
+									db = cmax;
+								} else if (tmph < 240.0f) {
+									db = cmin + (((cmax - cmin) * (240.0f - tmph)) / 60.0f);
+								} else {
+									db = cmin;
+								}
+
+								if (shusendo < 0.0f) {
+									lastDR = (int) (255.0f * dr);
+									lastDG = (int) (255.0f * dg);
+									lastDB = (int) (255.0f * db);
+								} else {
+									lastDR = (int) ((sr * shusendo) + (255.0f * dr * (1.0f - shusendo)));
+									lastDG = (int) ((sg * shusendo) + (255.0f * dg * (1.0f - shusendo)));
+									lastDB = (int) ((sb * shusendo) + (255.0f * db * (1.0f - shusendo)));
+								}
+								imageDataInt[off] = ((a << 24) & 0xff000000) | ((lastDR << 16) & 0xff0000) | ((lastDG << 8) & 0xff00) | (lastDB & 0xff);
+							}
+						}
+					}
+				}
+			}
+		}
+	}
 }
