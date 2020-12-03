7d204fb2873eda22309968c011a60ae2d4ef5561
Goujer <legojr33@gmail.com>
2020-12-03 08:23:58 -0600

    ServerConfigurationActivity is standalone
    No longer extends BaseActivity

--- src/main/java/com/goujer/barcodekanojo/activity/setting/ServerConfigurationActivity.kt
+++ src/main/java/com/goujer/barcodekanojo/activity/setting/ServerConfigurationActivity.kt
@@ -1,5 +1,7 @@
 package com.goujer.barcodekanojo.activity.setting
 
+import android.app.Activity
+import android.app.AlertDialog
 import android.os.Bundle
 import android.text.InputType
 import android.view.View
@@ -8,11 +10,10 @@ import android.widget.CompoundButton
 import android.widget.EditText
 import jp.co.cybird.barcodekanojoForGAM.BarcodeKanojoApp
 import jp.co.cybird.barcodekanojoForGAM.R
-import jp.co.cybird.barcodekanojoForGAM.activity.base.BaseEditActivity
-import jp.co.cybird.barcodekanojoForGAM.preferences.ApplicationSetting
+import com.goujer.barcodekanojo.preferences.ApplicationSetting
 import jp.co.cybird.barcodekanojoForGAM.view.EditItemView
 
-class ServerConfigurationActivity : BaseEditActivity(), View.OnClickListener {
+class ServerConfigurationActivity : Activity(), View.OnClickListener {
 	private lateinit var btnClose: Button
 	private lateinit var switchHttp: CompoundButton
 	private lateinit var txtURL: EditItemView
@@ -24,13 +25,13 @@ class ServerConfigurationActivity : BaseEditActivity(), View.OnClickListener {
 		btnClose = findViewById(R.id.setting_server_close)
 		btnClose.setOnClickListener(this)
 		switchHttp = findViewById(R.id.setting_server_https)
-		switchHttp.isChecked = ApplicationSetting(this).serverHttps
+		switchHttp.isChecked = ApplicationSetting(this).getServerHttps()
 		txtURL = findViewById(R.id.setting_server_url)
 		txtURL.setOnClickListener(this)
-		txtURL.value = ApplicationSetting(this).serverURL
+		txtURL.value = ApplicationSetting(this).getServerURL()
 		txtPort = findViewById(R.id.setting_server_port)
 		txtPort.setOnClickListener(this)
-		txtPort.value = ApplicationSetting(this).serverPort.toString()
+		txtPort.value = ApplicationSetting(this).getServerPort().toString()
 	}
 
 	override fun onDestroy() {
@@ -50,14 +51,19 @@ class ServerConfigurationActivity : BaseEditActivity(), View.OnClickListener {
 			R.id.setting_server_url -> {
 				input.setText(txtURL.value)
 				input.inputType = InputType.TYPE_CLASS_TEXT
-				showEditTextDialog(r.getString(R.string.server_url), txtURL, input)
+				showEditTextDialog(resources.getString(R.string.server_url), txtURL, input)
 			}
 			R.id.setting_server_port -> {
 				input.setText(txtPort.value)
 				input.inputType = InputType.TYPE_CLASS_NUMBER
-				showEditTextDialog(r.getString(R.string.server_port), txtPort, input)
+				showEditTextDialog(resources.getString(R.string.server_port), txtPort, input)
 			}
 		}
+	}
+
+	protected fun showEditTextDialog(title: String?, value: EditItemView, edit: EditText) {
+		val dialog = AlertDialog.Builder(this).setTitle(title).setView(edit).setPositiveButton(R.string.common_dialog_ok) { dialog, which -> value.value = edit.text.toString() }.setNegativeButton(R.string.common_dialog_cancel) { dialog, which -> }.create()
+		dialog.show()
 	}
 
 	override fun onBackPressed() {
