014bf7c8332c32fe96045121dc822e6edb4a8037
Goujer <legojr33@gmail.com>
2025-02-27 19:46:40 -0600

    Better rounded part of day calculation
    Now 9 and 3 are the starting and ending points.

--- app/src/main/java/com/goujer/utils/TimeUtil.kt
+++ app/src/main/java/com/goujer/utils/TimeUtil.kt
@@ -9,5 +9,7 @@ fun getPartOfDay(): Int {
 	val calendar = GregorianCalendar.getInstance()
 	calendar.time = Date()
 	val hour = calendar.get(Calendar.HOUR_OF_DAY)
-	return (((hour + 2) % 24 ) / 6)     // 0 - night, 1 - morning, 2 - day, 3 - evening
+	return (((hour + 3) % 24 ) / 6)     // 0 - night(9PM-3AM), 1 - morning(3AM-9AM), 2 - day(9AM-3PM), 3 - evening(3PM-9PM)
 }
+
+//Don't worry it is floored at the end, not rounded
